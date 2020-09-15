6cadf1fb0ca1ff7d6cf5e85ba70b142031bb3b98
Chris Coffin <me@chriscoffin.org>
2020-09-14 23:17:04 -0400

    Creating Swift Package

---
+++ Package.swift
@@ -0,0 +1,19 @@
+// swift-tools-version:5.3
+
+import PackageDescription
+
+let package = Package(
+    name: "swiftui-apnsManager",
+    products: [
+        .library(
+            name: "swiftui-apnsManager",
+            targets: ["swiftui-apnsManager"]),
+    ],
+    dependencies: [
+    ],
+    targets: [
+        .target(
+            name: "swiftui-apnsManager",
+            dependencies: []),
+    ]
+)
