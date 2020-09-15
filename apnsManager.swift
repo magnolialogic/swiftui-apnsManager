af5d15f5fc7369aed93ce9474210ab284a57d7bc
Chris Coffin <me@chriscoffin.org>
2020-09-14 22:57:58 -0400

    Adopting apiRoute environment variable

--- apnsManager.swift
+++ apnsManager.swift
@@ -27,7 +27,7 @@ class apnsManager: ObservableObject {
 	let dispatchGroup = DispatchGroup()
 	
 	// Root URL for python-apns_server API
-	let apiRoute = "https://apns.example.com/v1/user/"
+	let apiRoute = ProcessInfo.processInfo.environment["apiRoute"]
 	
 	
 	
