63246cac3e0a80c7f9780a17a7ca6394fa9da81c
Chris Coffin <me@chriscoffin.org>
2020-09-18 22:26:21 -0400

    Adopt Info.plist / xcconfig-based environment variable

--- Sources/APNSManager/apnsManager.swift
+++ Sources/APNSManager/apnsManager.swift
@@ -28,7 +28,7 @@ public class apnsManager: ObservableObject {
 	let dispatchGroup = DispatchGroup()
 	
 	// Root URL for python-apns_server API
-	let apiRoute = "https://" + Bundle.main.object(forInfoDictionaryKey: "API_ENDPOINT")! // If this isn't set we should crash, #@$&! it
+	let apiRoute = "https://" + Bundle.main.object(forInfoDictionaryKey: "API_ENDPOINT") as! String // If this isn't set we should crash, #@$&! it
 	
 	
 	
